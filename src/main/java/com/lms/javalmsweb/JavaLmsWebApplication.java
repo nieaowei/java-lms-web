0348e947b075c6e75c60f17ca7588a99e2d43a81
nieaowei <nieaowei@gmail.com>
2020-02-19 16:10:33 +0800

    Initial commit

---
+++ src/main/java/com/lms/javalmsweb/JavaLmsWebApplication.java
@@ -0,0 +1,13 @@
+package com.lms.javalmsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLmsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLmsWebApplication.class, args);
+    }
+
+}
