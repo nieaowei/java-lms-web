3647ab857f8f3b6deb66e8a8733018aee958d012
Hypnos_Lee <1642232305@qq.com>
2020-02-21 15:28:56 +0800

    完成了后端修改密码接口实现（含密码强度判断）

---
+++ src/main/java/com/lms/controller/PasswordController.java
@@ -0,0 +1,37 @@
+package com.lms.controller;
+
+
+import com.lms.entity.UserEntity;
+import com.lms.result.Result;
+import com.lms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PasswordController {
+    @Autowired
+    UserService userService;
+
+    @CrossOrigin
+    @PostMapping(value = "/user/changepd")
+    @ResponseBody
+    public Result Register(@RequestBody UserEntity requestUser){
+        int status = 200;
+        String msg = "修改成功";
+
+        UserEntity user = userService.getByName(requestUser.getUsername());
+        if(!userService.isPassWordCompliance(requestUser)){
+            status = 201;
+            msg = "密码不合格，应为8-16位数字字母混合";
+        }
+        else{
+            user.setPassword(requestUser.getPassword());
+            userService.save(user);
+        }
+        return new Result(status,msg);
+    }
+}
