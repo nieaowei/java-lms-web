e6810d606bf88cb59c02221c7d24715cbebaf521
liujunmin <1792619448@qqcom>
2020-02-23 00:31:52 +0800

    redis在线集成.jpg

--- src/main/java/com/lms/config/WebMvcConfig.java
+++ src/main/java/com/lms/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
-package com.lms.interception;
+package com.lms.config;
 
+import com.lms.interception.AuthorizationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- src/main/java/com/lms/controller/LoginController.java
+++ src/main/java/com/lms/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.lms.controller;
 import com.lms.entity.UserEntity;
 import com.lms.result.Result;
 import com.lms.service.UserService;
-import com.lms.utils.TokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
