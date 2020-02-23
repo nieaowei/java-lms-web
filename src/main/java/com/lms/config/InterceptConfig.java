90af9062699f189abec949d68eecd1b496849414
nieaowei <nieaowei@gmail.com>
2020-02-24 02:50:52 +0800

    实现了token拦截器注解模式。

---
+++ src/main/java/com/lms/config/InterceptConfig.java
@@ -0,0 +1,18 @@
+package com.lms.config;
+
+import com.lms.interception.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptConfig implements WebMvcConfigurer {
+    @Autowired
+    AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+    }
+}
