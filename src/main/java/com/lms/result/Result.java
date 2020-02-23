12b497b5a4809af7dd39a9f2f876746a58d51be3
nieaowei <nieaowei@gmail.com>
2020-02-24 02:52:30 +0800

    待解决泛型问题。

--- src/main/java/com/lms/result/Result.java
+++ src/main/java/com/lms/result/Result.java
@@ -1,6 +1,8 @@
 package com.lms.result;
 
-public class Result<T> {
+import java.io.Serializable;
+
+public class Result<T> implements Serializable {
     private int status;
     private String msg;
     private T data;
